4b385a3ed4fe3d40266c2230c89bbc98fb21c68e
fahmiam2 <fahmiam271@gmail.com>
2024-01-04 12:49:15 +0700

    fix(controllers): change allowed size file in MB and fix status code

--- app/controllers/image_detection.py
+++ app/controllers/image_detection.py
@@ -10,7 +10,6 @@ from fastapi import APIRouter, File, UploadFile, HTTPException, Request
 from fastapi.responses import JSONResponse
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
 import logging
 
 logger = logging.getLogger(__name__)
@@ -19,7 +18,7 @@ limiter = Limiter(key_func=get_remote_address)
 
 router = APIRouter()
 
-MAX_FILE_SIZE_MB = 10
+MAX_FILE_SIZE_MB = 15
 
 @router.post("/detect/image", 
              response_model=ImageDetectionResponse, 
@@ -38,7 +37,7 @@ async def detect_objects_in_image(
         logger.info("Received request to detect objects in an image.")
 
         if image.content_type not in ["image/jpg", "image/jpeg", "image/png"]:
-            raise HTTPException(status_code=422, detail="Invalid image format. Only JPG, JPEG and PNG are supported.")
+            raise HTTPException(status_code=415, detail="Invalid image format. Only JPG, JPEG and PNG are supported.")
 
         if image.file.seek(0, 2) > (MAX_FILE_SIZE_MB * 1024 * 1024):
             raise HTTPException(status_code=413, detail=f"File size exceeds the maximum allowed ({MAX_FILE_SIZE_MB} MB).")
@@ -51,13 +50,17 @@ async def detect_objects_in_image(
         result = await perform_object_detection(request_data)
 
         return result
-    except FileNotFoundError as file_not_found_error:
-        logger.error(f"File not found error: {file_not_found_error}")
-        raise HTTPException(status_code=400, detail=f"File not found: {file_not_found_error}")
+
     except HTTPException as e:
         if e.status_code == 422:
             logger.error(f"Validation error: {e.detail}")
-            return JSONResponse(content={"detail": "Validation error", "errors": e.detail}, status_code=422)
+            return JSONResponse(content={"error": "Validation error", "detail": e.detail}, status_code=422)
+        if e.status_code == 413:
+            logger.warning(f"Validation error: {e.detail}")
+            return JSONResponse(content={"error": "Validation error", "detail": e.detail}, status_code=413)
+        if e.status_code == 415:
+            logger.warning(f"Validation error: {e.detail}")
+            return JSONResponse(content={"error": "Validation error", "detail": e.detail}, status_code=415)
     except Exception as e:
         logger.exception(f"Internal server error: {e}")
         raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
--- app/controllers/video_detection.py
+++ app/controllers/video_detection.py
@@ -23,6 +23,7 @@ BASE_INPUT_FOLDER_PATH = "./temp/input_videos/"
 BASE_OUTPUT_FOLDER_PATH = "./temp/output_videos/"
 Path(BASE_INPUT_FOLDER_PATH).mkdir(parents=True, exist_ok=True)
 Path(BASE_OUTPUT_FOLDER_PATH).mkdir(parents=True, exist_ok=True)
+MAX_VIDEO_SIZE_MB = 15
 
 router = APIRouter()
 
@@ -37,9 +38,8 @@ async def save_video_locally(video: UploadFile) -> tuple[str, str]:
 
         allowed_content_types = ["video/mp4", "video/mpeg", "video/quicktime"]
         if video.content_type not in allowed_content_types:
-            raise HTTPException(status_code=422, detail="Invalid video file type. Supported types: " + ", ".join(allowed_content_types))
+            raise HTTPException(status_code=415, detail="Invalid video file type. Supported types: " + ", ".join(allowed_content_types))
 
-        MAX_VIDEO_SIZE_MB = 100
         size = await video.read()
         if len(size) > MAX_VIDEO_SIZE_MB * 1024 * 1024:
             raise HTTPException(status_code=413, detail=f"Video file size exceeds the maximum allowed ({MAX_VIDEO_SIZE_MB} MB).")
@@ -73,7 +73,7 @@ def delete_local_files(input_path: str, output_path: str) -> None:
              summary="Serves Object Detection in Video", 
              tags=["Video serve"],
              description="Performs object detection on an uploaded video and returns url of annotated video ")
-@limiter.limit("5/second")
+@limiter.limit("20/second")
 async def detect_objects_in_video(
     request: Request,
     video: UploadFile = File(...),
@@ -81,8 +81,11 @@ async def detect_objects_in_video(
     confidence_threshold: int = 25,
     annotator: str = "bounding_box",
     use_tracer: bool = False,
-    tracer: str = "tracer"
+    tracer: str = None
 ):
+    local_input_video_path = None
+    local_annotated_video_path = None
+
     try:
         logger.info("Received request to detect objects in an video.")
         filename, local_input_video_path = await save_video_locally(video)
@@ -99,16 +102,16 @@ async def detect_objects_in_video(
         result = await perform_video_detection(request_data, filename, source_path=local_input_video_path, target_path=local_annotated_video_path)
         return result
     
-    except FileNotFoundError as file_not_found_error:
-        logger.error(f"File not found error: {file_not_found_error}")
-        raise HTTPException(status_code=400, detail=f"File not found: {file_not_found_error}")
     except HTTPException as e:
         if e.status_code == 422:
             logger.warning(f"Validation error: {e.detail}")
-            return JSONResponse(content={"detail": "Validation error", "errors": e.detail}, status_code=422)
+            return JSONResponse(content={"error": "Validation error", "detail": e.detail}, status_code=422)
         if e.status_code == 413:
             logger.warning(f"Validation error: {e.detail}")
-            return JSONResponse(content={"detail": "Validation error", "errors": e.detail}, status_code=413)
+            return JSONResponse(content={"error": "Validation error", "detail": e.detail}, status_code=413)
+        if e.status_code == 415:
+            logger.warning(f"Validation error: {e.detail}")
+            return JSONResponse(content={"error": "Validation error", "detail": e.detail}, status_code=415)
     except Exception as e:
         logger.exception(f"Internal server error: {e}")
         raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
