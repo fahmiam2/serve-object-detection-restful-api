ef307e48c38d7747319590bd9ee356a576d73082
fahmiam2 <fahmiam271@gmail.com>
2024-01-10 16:07:59 +0700

    fix(video): fix get secret for generating signed url in cloud run

--- app/services/video_detection.py
+++ app/services/video_detection.py
@@ -9,27 +9,64 @@ from app.schemas.detection_schema import VideoDetectionRequest
 from app.validation.video_detection import validate_video_detection_request
 from model.yolov8s.video_detection_model import YoloV8VideoObjectDetection
 from fastapi.responses import JSONResponse
+from google.auth.transport import requests
 from google.cloud import storage
 from config.settings import GCS_KEY_FILE, GCS_BUCKET_NAME
+import google.auth
 import logging
 import supervision as sv
 
 logger = logging.getLogger(__name__)
 
-def upload_to_gcs(bucket_name, source_file_path, object_name, credential_path) -> None:
-    client = storage.Client.from_service_account_json(credential_path)
+# Global variable to store the GCS client
+gcs_client = None
+
+def get_gcs_client(credential_path=None):
+    global gcs_client
+    if gcs_client is None:
+        try:
+            if credential_path:
+                gcs_client = storage.Client.from_service_account_json(credential_path)
+            else:
+                gcs_client = storage.Client()
+        except Exception as e:
+            logger.error(f"Error creating GCS client: {e}")
+            raise
+    return gcs_client
+
+def upload_to_gcs(bucket_name, source_file_path, object_name, credential_path=None) -> None:
+    client = get_gcs_client(credential_path)
+
+    if client is None:
+        raise ValueError("GCS client must be provided.")
+
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(object_name)
     blob.upload_from_filename(source_file_path)
 
-def generate_signed_url(bucket_name, object_name, credential_path, expiration=3600) -> str:
-    client = storage.Client.from_service_account_json(credential_path)
+    return client
+
+def generate_signed_url(bucket_name, object_name, credential_path=None, expiration=3600) -> str:
+    
+    if credential_path is None:
+        credentials, _ = google.auth.default()
+        r = requests.Request()
+        credentials.refresh(r)
+
+        client = storage.Client()
+    else:
+        client = storage.Client.from_service_account_json(credential_path)
+
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(object_name)
+    service_account_email = credentials.service_account_email
+
     url = blob.generate_signed_url(
+        service_account_email=service_account_email,
+        access_token=credentials.token,
         version="v4",
-        expiration=expiration,
         method="GET",
+        expiration=expiration,
     )
     return url
 
@@ -42,7 +79,8 @@ async def perform_video_detection(request_data: VideoDetectionRequest, filename,
     
     logger.info("Performing object detection")
     source_video_info = sv.VideoInfo.from_video_path(video_path=source_path) 
-    with sv.VideoSink(target_path=target_path, video_info=source_video_info, codec="h264") as sink:
+
+    with sv.VideoSink(target_path=target_path, video_info=source_video_info, codec="vp09") as sink:
         for index, frame in enumerate(
             sv.get_video_frames_generator(source_path=source_path)
         ):
@@ -61,12 +99,13 @@ async def perform_video_detection(request_data: VideoDetectionRequest, filename,
     logger.info(f"Uploading {filename} to GCS")
     # Additional logic to upload annotated frames to GCS
     gcs_object_name = f"{filename}"  # Adjust the naming as needed
-    upload_to_gcs(bucket_name=GCS_BUCKET_NAME, source_file_path=target_path, object_name=gcs_object_name, credential_path=GCS_KEY_FILE)
+    client = upload_to_gcs(bucket_name=GCS_BUCKET_NAME, source_file_path=target_path, object_name=gcs_object_name, credential_path=GCS_KEY_FILE)
     logger.info(f"Successfully uploading {filename} to gcs")
+    logger.info(f"here is the client: {client}")
 
     logger.info(f"Generate signed URL for accessing file: {filename}")
     # Additional logic to generate signed URL for the video
-    signed_url = generate_signed_url(bucket_name=GCS_BUCKET_NAME, object_name=gcs_object_name, credential_path=GCS_KEY_FILE)
+    signed_url = generate_signed_url(bucket_name=GCS_BUCKET_NAME, object_name=gcs_object_name)
     logger.info("Signed url has successfully created")
     
     return JSONResponse(content={"url_video": signed_url}, status_code=200)
