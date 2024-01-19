48c317a4fe2177cab8752ac45274c7f4cfba369c
fahmiam2 <fahmiam271@gmail.com>
2024-01-19 11:40:20 +0700

    refactor: add type hints and align the output format file with input format file

--- app/services/image_detection.py
+++ app/services/image_detection.py
@@ -8,24 +8,29 @@ from app.schemas.detection_schema import ImageDetectionRequest, ImageDetectionRe
 from app.validation.image_detection import validate_object_detection_request
 from model.yolov8s.image_detection_model import YoloV8ImageObjectDetection
 import cv2
-import numpy as np
 from fastapi.responses import JSONResponse
 import base64
 
-async def perform_object_detection(request_data: ImageDetectionRequest) -> JSONResponse:
+async def perform_object_detection(request_data: ImageDetectionRequest, content_type: str) -> JSONResponse:
     validate_object_detection_request(request_data)
     
     yolo_model = YoloV8ImageObjectDetection(chunked=request_data.image, task_type=request_data.task_type)
     annotated_image, object_counts = await yolo_model(conf_threshold=request_data.confidence_threshold)
 
     # Convert annotated image to base64
-    _, img_encoded = cv2.imencode('.jpg', annotated_image)
+    if content_type.lower() in ["image/jpg", "image/jpeg"]:
+        _, img_encoded = cv2.imencode('.jpg', annotated_image)
+    elif content_type.lower() == "image/png":
+        _, img_encoded = cv2.imencode('.png', annotated_image)
+    else:
+        return JSONResponse(content={"error": "Unsupported content type"}, status_code=400)
+
     img_base64 = base64.b64encode(img_encoded).decode('utf-8')
 
     # Prepare response data
     response_data = {
-        "frame": img_base64,
-        "object_counts": object_counts
+        "object_counts": object_counts,
+        "frame": img_base64
     }
 
     return JSONResponse(content=response_data, status_code=200)
