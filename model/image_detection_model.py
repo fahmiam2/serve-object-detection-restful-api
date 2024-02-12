072258069e2f01d3787cb23a4c78dfbd4ba2e167
fahmiam2 <fahmiam271@gmail.com>
2024-02-12 20:05:33 +0700

    chore: move image and video detection model scripts

---
+++ model/image_detection_model.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+import sys
+
+root_directory = Path(__file__).resolve().parents[1]
+sys.path.append(str(root_directory))
+
+from ultralytics import YOLO
+import cv2
+import logging
+import platform
+import numpy as np
+import supervision as sv
+import torch
+from typing import List, Tuple, Dict, Union
+
+logger = logging.getLogger(__name__)
+
+class YoloV8ImageObjectDetection:
+    MODEL_PATHS = {
+        "detection": {
+            "yolov8n": "./model/yolov8n/yolov8n.pt",
+            "yolov8s": "./model/yolov8s/yolov8s.pt",
+            "yolov8m": "./model/yolov8m/yolov8m.pt",
+            "yolov8l": "./model/yolov8l/yolov8l.pt",
+            "yolov8x": "./model/yolov8x/yolov8x.pt"
+        },
+        "segmentation": {
+            "yolov8n": "./model/yolov8n/yolov8n-seg.pt",
+            "yolov8s": "./model/yolov8s/yolov8s-seg.pt",
+            "yolov8m": "./model/yolov8m/yolov8m-seg.pt",
+            "yolov8l": "./model/yolov8l/yolov8l-seg.pt",
+            "yolov8x": "./model/yolov8x/yolov8x-seg.pt"
+        }
+    }
+
+    def __init__(self, chunked: bytes = None, task_type: str = "detection", model_type: str = "yolov8s"):
+        self._bytes = chunked
+        self._task_type = task_type
+        self._model_type = model_type
+        self.PATH = self._get_model_path()
+        self.model = self._load_model()
+        self.device = self._get_device()
+        self.classes = self.model.names
+
+    def _get_model_path(self) -> str:
+        if self._task_type in YoloV8ImageObjectDetection.MODEL_PATHS and \
+                self._model_type in YoloV8ImageObjectDetection.MODEL_PATHS[self._task_type]:
+            return YoloV8ImageObjectDetection.MODEL_PATHS[self._task_type][self._model_type]
+        else:
+            raise ValueError(f"Unsupported task_type: {self._task_type} or model_type: {self._model_type}")
+
+    def _get_device(self) -> str:
+        if platform.system().lower() == "darwin":
+            return "mps"
+        if torch.cuda.is_available():
+            return "cuda"
+        return "cpu"
+
+    def _load_model(self) -> YOLO:
+        try:
+            logger.info(f"Loading YOLO model with task type: {self._task_type}")
+            model = YOLO(self.PATH)
+            return model
+        except Exception as e:
+            logger.exception(f"Error loading YOLO model: {e}")
+            raise e
+    
+    async def __call__(self, conf_threshold: int = 25) -> Tuple[np.ndarray, Dict[str, int]]:
+        try:
+            logger.info("Performing object detection")
+            input_image = self._get_image_from_chunked()
+            results = self.model.predict(input_image, agnostic_nms=True, verbose=False)[0]
+            detections, labels, object_counts = self._get_results_from_detections(results, conf_threshold / 100.0)
+            annotated_image = self._annotate_object_detections(input_image, labels, detections)
+            logger.info("Object detection successfully completed")
+            return annotated_image, object_counts
+        except Exception as e:
+            logger.exception(f"Error during object detection: {e}")
+            raise e
+    
+    def _get_image_from_chunked(self) -> np.ndarray:
+        arr = np.asarray(bytearray(self._bytes), dtype=np.uint8)
+        img = cv2.imdecode(arr, -1)
+        return img
+    
+    def _get_results_from_detections(self, detections: object, conf_threshold: float) -> Tuple[object, List[str], Dict[str, int]]:
+        detections_sv = sv.Detections.from_ultralytics(detections)
+        filtered_detections = detections_sv[detections_sv.confidence >= conf_threshold]
+        label_detections = self._get_labels_objects(filtered_detections)
+        detected_classes = [self.classes[class_id] for class_id in filtered_detections.class_id]
+        object_counts = self._count_objects(detected_classes)
+
+        return filtered_detections, label_detections, object_counts
+    
+    def _get_labels_objects(self, detections: object) -> List[str]:
+        labels = [
+            f"{self.classes[class_id]} ({confidence*100:0.2f}%)"
+            for confidence, class_id
+            in zip(detections.confidence, detections.class_id)
+        ]
+        
+        return labels
+
+    def _count_objects(self, object_list: List[str]) -> Dict[str, int]:
+        class_counts = {}
+        for obj in object_list:
+            class_counts[obj] = class_counts.get(obj, 0) + 1
+        return class_counts
+    
+    def _annotate_object_detections(self, image, labels: List[str], detections: object) -> np.ndarray:
+        try:
+            logger.info("Annotating object detections.")
+            if self._task_type == "detection":
+                bounding_box_annotator = sv.BoundingBoxAnnotator()
+                annotated_image = bounding_box_annotator.annotate(
+                    scene=image, 
+                    detections=detections
+                )
+            elif self._task_type == "segmentation":
+                mask_annotator = sv.MaskAnnotator()
+                annotated_image = mask_annotator.annotate(
+                    scene=image, 
+                    detections=detections
+                )
+            else:
+                raise ValueError(f"Unsupported task_type: {self._task_type}")
+            
+            # for large image >> text_scale=5, text_padding=60, text_thickness=5
+            label_annotator = sv.LabelAnnotator()
+
+            annotated_image = label_annotator.annotate(
+                scene=annotated_image, detections=detections, labels=labels
+            )
+
+            return annotated_image
+
+        except Exception as e:
+            logger.exception(f"Error during annotation: {e}")
---
+++ model/video_detection_model.py
@@ -0,0 +1,167 @@
+from pathlib import Path
+import sys
+
+root_directory = Path(__file__).resolve().parents[1]
+sys.path.append(str(root_directory))
+
+from typing import Any
+from ultralytics import YOLO
+import logging
+import platform
+import numpy as np
+import supervision as sv
+import torch
+
+logger = logging.getLogger(__name__)
+
+tracker = sv.ByteTrack()
+label_annotator = sv.LabelAnnotator()
+trace_annotator = sv.TraceAnnotator()
+
+class YoloV8VideoObjectDetection:
+    CONF_THRESHOLD_DEFAULT = 25
+    DETECTION_PATH = "./model/yolov8s/yolov8s.pt"
+    SEGMENTATION_PATH = "./model/yolov8s/yolov8s-seg.pt"
+
+    ANNOTATOR_TYPE_DETECTION = {
+        "bounding_box": sv.BoundingBoxAnnotator(),
+        "box_corner": sv.BoxCornerAnnotator(),
+        "color": sv.ColorAnnotator(),
+        "circle": sv.CircleAnnotator(),
+        "dot": sv.DotAnnotator(),
+        "triangle": sv.TriangleAnnotator(),
+        "ellipse": sv.EllipseAnnotator(),
+    }
+
+    ANNOTATOR_TYPE_SEGMENTATION = {
+        "bounding_box": sv.BoundingBoxAnnotator(),
+        "box_corner": sv.BoxCornerAnnotator(),
+        "color": sv.ColorAnnotator(),
+        "circle": sv.CircleAnnotator(),
+        "dot": sv.DotAnnotator(),
+        "triangle": sv.TriangleAnnotator(),
+        "ellipse": sv.EllipseAnnotator(),
+        "halo": sv.HaloAnnotator(),
+        "mask": sv.MaskAnnotator(),
+        "polygon": sv.PolygonAnnotator(),
+    }
+
+    TRACER_TYPE = {
+        "tracer": sv.TraceAnnotator(),
+        "heatmap": sv.HeatMapAnnotator()
+    }
+
+    def __init__(self, task_type: str = "detection") -> None:
+        self._validate_task_type(task_type)
+        self._task_type = task_type
+        self.PATH = self._get_model_path()
+        self.model = self._load_model()
+        self.device = self._get_device()
+        self.classes = self.model.names
+
+    def _validate_task_type(self, task_type: str) -> None:
+        """Validate the provided task type."""
+        if task_type not in ["detection", "segmentation"]:
+            raise ValueError(f"Invalid task_type: {task_type}")
+
+    def _get_model_path(self):
+        """Get the appropriate model path based on task_type."""
+        if self._task_type == "detection":
+            return YoloV8VideoObjectDetection.DETECTION_PATH
+        elif self._task_type == "segmentation":
+            return YoloV8VideoObjectDetection.SEGMENTATION_PATH
+        else:
+            raise ValueError(f"Unsupported task_type: {self._task_type}")
+    
+    def _get_device(self):
+        """Gets best device for your system
+
+        Returns:
+            device (str): The device to use for YOLO for your system
+        """
+        if platform.system().lower() == "darwin":
+            return "mps"
+        if torch.cuda.is_available():
+            return "cuda"
+        return "cpu"
+    
+    def _load_model(self):
+        """Loads Yolo8 model from pytorch hub or a path on disk
+
+        Returns:
+            model (Model) - Trained Pytorch model
+        """
+        try:
+            logger.info(f"Loading YOLO model with task type: {self._task_type}")
+            model = YOLO(self.PATH)
+            return model
+        except Exception as e:
+            logger.exception(f"Error loading YOLO model: {e}")
+            raise e
+    
+    def callback(self, frame: np.ndarray, _: int, conf_threshold: int = 25, annotator: str = "bounding_box", use_tracer: bool = False, tracer: str = None) -> Any:
+        try:
+            if annotator not in ["bounding_box", "box_corner", "color", "circle", "dot", "triangle", "ellipse", "halo", "mask", "polygon"]:
+                raise ValueError(f"Invalid annotator type: {annotator}")
+
+            results = self.model(frame)[0]
+            detections_sv = sv.Detections.from_ultralytics(results)
+            detections = detections_sv[detections_sv.confidence >= conf_threshold / 100]
+
+            # annotating
+            annotated_frame = self._get_annotator_instance(frame, detections, annotator)
+
+            # labelling
+            detections = tracker.update_with_detections(detections)
+
+            labels = [
+                f"#{tracker_id} {results.names[class_id]} ({confidence*100:0.2f}%)"
+                for class_id, tracker_id, confidence
+                in zip(detections.class_id, detections.tracker_id, detections.confidence)
+            ]
+            
+            annotated_frame = self._annotate_with_label(annotated_frame, detections, labels)
+            
+            # tracing
+            if use_tracer and tracer is not None:
+                if tracer not in ["tracer", "heatmap"]:
+                    raise ValueError(f"Invalid tracer type: {tracer}")
+                annotated_frame = self._annotate_with_tracer(annotated_frame, tracer, detections)
+                
+            return annotated_frame
+
+        except Exception as e:
+            logger.exception(f"Error during object detection callback: {e}")
+            raise e
+
+    
+    def _get_annotator_instance(self, frame, detections, annotator):
+        if isinstance(annotator, str):
+            if annotator in self.ANNOTATOR_TYPE_DETECTION and self._task_type == "detection":
+                annotator_instance =  self.ANNOTATOR_TYPE_DETECTION[annotator]
+            elif annotator in self.ANNOTATOR_TYPE_SEGMENTATION and self._task_type == "segmentation":
+                annotator_instance = self.ANNOTATOR_TYPE_SEGMENTATION[annotator]
+            else:
+                raise ValueError(f"Unsupported annotator for task_type: {annotator} for {self._task_type}")
+            
+            annotate_frame = annotator_instance.annotate(
+                    scene=frame.copy(),
+                    detections=detections
+                )
+            return annotate_frame
+        else:
+            raise ValueError("Invalid annotator type")
+        
+    def _annotate_with_label(self, annotated_frame, detections, labels):
+        label_annotator = sv.LabelAnnotator()
+        return label_annotator.annotate(
+            annotated_frame, detections=detections, labels=labels
+        )
+        
+    def _annotate_with_tracer(self, annotated_frame, tracer, detections):
+        if tracer is not None and isinstance(tracer, str) and tracer in self.TRACER_TYPE:
+            tracer_instance = self.TRACER_TYPE[tracer]
+            return tracer_instance.annotate(
+                annotated_frame, detections=detections
+            )
+        return annotated_frame
